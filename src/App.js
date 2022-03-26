5cfb10c3f14827759faf49af9a90e958032a934e
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-03-26 20:59:12 -0300

    add gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios("http://api.openweathermap.org/data/2.5/weather", {
+      params: {
+        lat: lat,
+        lon: long,
+        appid: "debb98d2ccf2eed3b85b93a8c4d380bd",
+        lang: "pt",
+        units: "metric",
+      },
+    });
+    setWeather(res.data);
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  if (location == false) {
+    return <>Sua localização precisa estar habilitada</>;
+  } else if (weather == false) {
+    return <>Carregando...</>;
+  } else {
+    <>
+      <h3>
+        Veja o tempo nas suas coordenadas({weather["weather"][0]["description"]}
+        )
+      </h3>
+      <hr />
+      <ul>
+        <li>Temperatura atual: {weather["main"]["temp"]}º</li>
+        <li>Temperatura máxima: {weather["main"]["temp_max"]}º</li>
+        <li>Temperatura mínima: {weather["main"]["temp_min"]}º</li>
+        <li>Pressão: {weather["main"]["pressure"]}hpa</li>
+        <li>Umidade: {weather["main"]["humidity"]}%</li>
+      </ul>
+    </>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>
+        Veja o tempo nas suas coordenadas({weather["weather"][0]["description"]}
+        )
+      </h3>
+      <hr />
+      <ul>
+        <li>Temperatura atual: {weather["main"]["temp"]}º</li>
+        <li>Temperatura máxima: {weather["main"]["temp_max"]}º</li>
+        <li>Temperatura mínima: {weather["main"]["temp_min"]}º</li>
+        <li>Pressão: {weather["main"]["pressure"]}hpa</li>
+        <li>Umidade: {weather["main"]["humidity"]}%</li>
+      </ul>
+    </>
   );
 }
 
