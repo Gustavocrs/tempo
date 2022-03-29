d28ff4b8ce5cbfaa33c5b281dd6896c80b53e81d
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-03-29 19:26:32 -0300

    inclusao de loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+import { useEffect, useState } from 'react'
+
 import './App.css'
+
+import { GoLocation } from 'react-icons/go'
 import {
   WiThermometerExterior,
   WiThermometer,
@@ -34,17 +38,22 @@ function App() {
   }, [])
 
   if (location === false) {
-    return <>Sua localização precisa estar habilitada</>
+    return (
+      <div className="card">
+        <GoLocation className="icon" />
+        <h1>Sua localização precisa estar habilitada</h1>
+      </div>
+    )
   } else if (weather === false) {
     return (
-      <>
+      <div className="card">
         <div className="ldsEllipsis">
           <div></div>
           <div></div>
           <div></div>
           <div></div>
         </div>
-      </>
+      </div>
     )
   } else {
     return (
@@ -53,38 +62,46 @@ function App() {
           <h1>Localização estimada:</h1>
           <p className="infos">{weather['name']}</p>
           <div className="lista">
-            <p className="principal">{weather['main']['temp'].toFixed(1)}º</p>
-            {/* <p className='secundario'>{weather['weather'][0]['description']}</p> */}
-            <p className="infos">
-              Sensação térmica: {weather['main']['feels_like'].toFixed(1)}º
-            </p>
+            <div>
+              <p className="principal">{weather['main']['temp'].toFixed(1)}º</p>
+              <p className="infos">
+                Sensação térmica: {weather['main']['feels_like'].toFixed(1)}º
+              </p>
+            </div>
             <div className="divList">
               <div className="divIcons">
-                <WiThermometer className="icon" /><p className='miniDesc'>Máxima</p>
+                <WiThermometerExterior className="icon" />
+                <p className="iconDesc">
+                  <p className="miniDesc">Minima</p>
+                  {weather['main']['temp_min'].toFixed(1)}º
+                </p>
+              </div>
+              <div className="divIcons">
+                <WiThermometer className="icon" />
+                <p className="miniDesc">Máxima</p>
                 <p className="iconDesc">
                   {weather['main']['temp_max'].toFixed(1)}º
                 </p>
               </div>
               <div className="divIcons">
-                <WiThermometerExterior className="icon" />
-                <p className="iconDesc"><p className='miniDesc'>Minima</p>
-                  {weather['main']['temp_min'].toFixed(1)}º
-                </p>
-              </div>
-              <div className="divIcons">
                 <WiHumidity className="icon" />
-                <p className="iconDesc"><p className='miniDesc'>Umidade</p>
+                <p className="iconDesc">
+                  <p className="miniDesc">Umidade</p>
                   {weather['main']['humidity'].toFixed(0)}%
                 </p>
               </div>
               <div className="divIcons">
                 <WiStrongWind className="icon" />
-                <p className="iconDesc"><p className='miniDesc'>Ventos</p>
+                <p className="iconDesc">
+                  <p className="miniDesc">Ventos</p>
                   {weather['wind']['speed'].toFixed(1)}km/h
                 </p>
               </div>
             </div>
             {/* <li>Pressão: {weather['main']['pressure']}hpa</li> */}
+            {/* <p className='secundario'>{weather['weather'][0]['description']}</p> */}
+            <p>Fonte: OpenWeather</p>
+            <p>https://openweathermap.org/</p>
           </div>
         </div>
       </section>
