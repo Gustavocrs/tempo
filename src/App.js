3e5809348d36868b087f0abbdfade42bfeff1a91
Gustavocrs <86523009+Gustavocrs@users.noreply.github.com>
2022-03-28 22:27:14 -0300

    inclusão de css

--- src/App.js
+++ src/App.js
@@ -1,54 +1,95 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import "./App.css";
+import React from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+import {
+  WiThermometerExterior,
+  WiThermometer,
+  WiHumidity,
+  WiStrongWind,
+} from 'react-icons/wi'
 
 function App() {
-  const [location, setLocation] = useState(false);
-  const [weather, setWeather] = useState(false);
+  const [location, setLocation] = useState(false)
+  const [weather, setWeather] = useState(false)
 
   let getWeather = async (lat, long) => {
-    let res = await axios("http://api.openweathermap.org/data/2.5/weather", {
+    let res = await axios('https://api.openweathermap.org/data/2.5/weather', {
       params: {
         lat: lat,
         lon: long,
-        appid: "debb98d2ccf2eed3b85b93a8c4d380bd",
-        lang: "pt",
-        units: "metric",
+        appid: 'debb98d2ccf2eed3b85b93a8c4d380bd',
+        lang: 'pt',
+        units: 'metric',
       },
-    });
-    setWeather(res.data);
-  };
+    })
+    setWeather(res.data)
+  }
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      getWeather(position.coords.latitude, position.coords.longitude);
-      setLocation(true);
-    });
-  }, []);
+      getWeather(position.coords.latitude, position.coords.longitude)
+      setLocation(true)
+    })
+  }, [])
 
   if (location === false) {
-    return <>Sua localização precisa estar habilitada</>;
+    return <>Sua localização precisa estar habilitada</>
   } else if (weather === false) {
-    return <>Carregando...</>;
-  } else {
     return (
       <>
-        <h3>Localização estimada: "{weather["name"]}"</h3>
-        <hr />
-        <ul>
-          <li>Visão do tempo: {weather["weather"][0]["description"]} </li>
-          <li>Temperatura atual: {weather["main"]["temp"]}º</li>
-          <li>Sensação térmica: {weather["main"]["feels_like"]}º</li>
-          <li>Temperatura máxima: {weather["main"]["temp_max"]}º</li>
-          <li>Temperatura mínima: {weather["main"]["temp_min"]}º</li>
-          <li>Pressão: {weather["main"]["pressure"]}hpa</li>
-          <li>Umidade: {weather["main"]["humidity"]}%</li>
-          <li>Ventos: {weather["wind"]["speed"]}km/h</li>
-        </ul>
+        <div className="ldsEllipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </>
-    );
+    )
+  } else {
+    return (
+      <section>
+        <div className="card">
+          <h1>Localização estimada:</h1>
+          <p className="infos">{weather['name']}</p>
+          <div className="lista">
+            <p className="principal">{weather['main']['temp'].toFixed(1)}º</p>
+            {/* <p className='secundario'>{weather['weather'][0]['description']}</p> */}
+            <p className="infos">
+              Sensação térmica: {weather['main']['feels_like'].toFixed(1)}º
+            </p>
+            <div className="divList">
+              <div className="divIcons">
+                <WiThermometer className="icon" /><p className='miniDesc'>Máxima</p>
+                <p className="iconDesc">
+                  {weather['main']['temp_max'].toFixed(1)}º
+                </p>
+              </div>
+              <div className="divIcons">
+                <WiThermometerExterior className="icon" />
+                <p className="iconDesc"><p className='miniDesc'>Minima</p>
+                  {weather['main']['temp_min'].toFixed(1)}º
+                </p>
+              </div>
+              <div className="divIcons">
+                <WiHumidity className="icon" />
+                <p className="iconDesc"><p className='miniDesc'>Umidade</p>
+                  {weather['main']['humidity'].toFixed(0)}%
+                </p>
+              </div>
+              <div className="divIcons">
+                <WiStrongWind className="icon" />
+                <p className="iconDesc"><p className='miniDesc'>Ventos</p>
+                  {weather['wind']['speed'].toFixed(1)}km/h
+                </p>
+              </div>
+            </div>
+            {/* <li>Pressão: {weather['main']['pressure']}hpa</li> */}
+          </div>
+        </div>
+      </section>
+    )
   }
 }
 
-export default App;
+export default App
